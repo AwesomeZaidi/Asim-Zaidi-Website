f79ac7c7e4fe49e7fbc80925d322e35442dc0ba9
Asim Zaidi <asimzaidih@gmail.com>
2018-11-14 02:59:14 -0800

    scss media breakpoint mixins mobile, tablet, desktop

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars');
+const port = process.env.PORT || 3000
+const path = require('path') // research the path native node module
+const bodyParser = require('body-parser')
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('hbs', exphbs({
+  defaultLayout: "main",
+  extname: ".hbs",
+  helpers: require("handlebars-helpers")(),
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+require('./data/personalwebsite-db.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('index');
+});
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
